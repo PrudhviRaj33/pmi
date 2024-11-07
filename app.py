f624d5e0806ce7c9379fe90791e005210fe8e9e0
Prudhvi Raj <128157654+PrudhviRaj33@users.noreply.github.com>
2024-11-07 23:20:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,5 +120,4 @@ def download_file(filename):
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
-    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
